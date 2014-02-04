b8fd34310e633b507bcf99adcf64acc32f87a1f8
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2014-02-04 09:14:44 +0200

    Add client.

---
+++ publicatorclient.py
@@ -0,0 +1,56 @@
+import logging
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+# if there is no handler run this code
+if not logger.handlers:
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+
+class PublicatorClient(object):
+    """
+    Client objects that communicates with publicator server.
+    """
+
+    HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    SESSION_URI = 'session/'
+    MESSAGE_URI = 'messages/'
+
+    def __init__(self, base_url):
+        self.base_url = '{0}{1}'.format(base_url,
+                                        ('' if base_url[-1] == '/' else '/'))
+        self.session_id = self.get_session()
+
+    def get_session(self):
+        """
+        Creates a new session on publicator server
+        """
+        url = '{0}{1}'.format(self.base_url, self.SESSION_URI)
+        logger.debug('Going to server %s to create a session', url)
+        response = requests.get(url, headers=self.HEADERS)
+        response.raise_for_status()
+        result = response.json()
+        logger.debug('Session is response=%s', result)
+        return result['session']
+
+    def publish(self, channel, msg):
+        """
+        Publishes a message to given channel
+        """
+        url = '{0}{1}/{2}{3}/'.format(self.base_url,
+                                      self.session_id,
+                                      self.MESSAGE_URI,
+                                      channel)
+        logger.debug('Going to server %s to publish a message', url)
+
+        response = requests.post(url,
+                                 data={'message': msg},
+                                 headers=self.HEADERS)
+        response.raise_for_status()
+        # XXX
