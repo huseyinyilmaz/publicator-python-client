74e0963e1bce4fe7ff58dbd74b10c3861965437c
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2014-02-26 09:52:29 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='publicatorclient',
+    version='0.1.1',
+    author='Huseyin Yilmaz',
+    author_email='yilmazhuseyin@gmail.com',
+    url='https://github.com/huseyinyilmaz/publicator-python-client',
+    description='Client for connecting publicator server.',
+    long_description=os.path.join(os.path.dirname(__file__), 'README.md'),
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        'requests==2.2.1'
+    ],
+    include_package_data=True,
+)
